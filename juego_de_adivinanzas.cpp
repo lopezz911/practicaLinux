5d4c45f4c2d6247ba1db84daf040c34566e5c48e
lopezz911 <sergio.lopez.c1@ucb.edu.bo>
2026-03-20 13:14:12 -0400

    2026-03-20_01 subir archivo cpp

---
+++ juego_de_adivinanzas.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "*************************************" << endl;
+	cout << "Bienvenido al Juego de la Adivinanza!" << endl;
+	cout << "*************************************" << endl;
+
+	return 0;
+}
